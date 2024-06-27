sample school coordinates from grid to guarantee schools are not too close to each other, fix variance computation

src/data.py
@@ -4,6 +4,8 @@ import pickle
 import networkx as nx
 from scipy.spatial.distance import pdist, squareform
 
+from matplotlib import pyplot as plt
+
 
 def gen_multarm_data(n_students, tau_sizes, sigma, seed=42):
     rng = np.random.default_rng(seed)
@@ -76,11 +78,17 @@ def gen_kenya_network(
     rng = np.random.default_rng(seed)
     all_sch_coords = []
     for coords in set_coords:
-        sch_coords_0 = rng.uniform(coords[0] - coords_range, coords[0] + coords_range, n_sch_per_set)
-        sch_coords_1 = rng.uniform(coords[1] - coords_range, coords[1] + coords_range, n_sch_per_set)
+        sch_coords_0 = rng.choice(np.linspace(coords[0] - coords_range, coords[0] + coords_range, 200), n_sch_per_set, replace=False)
+        sch_coords_1 = rng.choice(np.linspace(coords[1] - coords_range, coords[1] + coords_range, 200), n_sch_per_set, replace=False)
+        # sch_coords_0 = rng.uniform(coords[0] - coords_range, coords[0] + coords_range, n_sch_per_set)
+        # sch_coords_1 = rng.uniform(coords[1] - coords_range, coords[1] + coords_range, n_sch_per_set)
         sch_coords = np.vstack((sch_coords_0, sch_coords_1)).T
         all_sch_coords.append(sch_coords)
     all_sch_coords = np.vstack(all_sch_coords)
+    
+    fig, ax = plt.subplots()
+    ax.scatter(all_sch_coords[:, 0], all_sch_coords[:, 1])
+    fig.savefig("sch_coords.png")
 
     n_sch = n_sch_per_set * len(set_coords)
     w_mat = np.ones((n_sch, n_sch))

src/metrics.py
@@ -98,9 +98,8 @@ def InvMinEuclidDist(z_pool, D):
         Z_diff_mask = np.matmul(z.T, 1 - z) + np.matmul((1 - z).T, z)
 
         # calculate minimum pairwise Euclidean distance between units assigned to different arms
+        # and take inverse
         D_diff_z = D[Z_diff_mask.astype(bool)]
-
-        # take inverse
         min_D[i] = 1 / np.min(D_diff_z)
 
     return min_D

vignettes/interference_figs.py
@@ -122,6 +122,11 @@ def interference_bias_rmse_rr_vs_weight(
     ax[1].get_legend().set_visible(False)
 
     # Rejection rate
+    cr_rr = res[res['design'] == 'CR']['rr']
+    ax[2].axhline(cr_rr.mean(), color=color_mapping("CR"), linewidth=3, label="CR")
+    for rr in cr_rr:
+        ax[2].axhline(rr, color=color_mapping("CR"), linewidth=1, alpha=0.5)
+
     sns.lineplot(
         data=res_igr,
         x="w_balance",
@@ -151,17 +156,13 @@ def interference_bias_rmse_rr_vs_weight(
         ax=ax[2],
         errorbar=None,
     )
-    cr_rr = res[res['design'] == 'CR']['rr']
-    ax[2].axhline(cr_rr.mean(), color=color_mapping("CR"), linewidth=3, label="CR")
-    for rr in cr_rr:
-        ax[2].axhline(rr, color=color_mapping("CR"), linewidth=1, alpha=0.5)
     ax[2].set_xlabel(r"$w_{MaxMahalanobis}$", fontsize=18)
     ax[2].set_ylabel("Rejection Rate", fontsize=18)
     ax[2].tick_params(axis="x", labelsize=16)
     ax[2].tick_params(axis="y", labelsize=16)
     ax[2].get_legend().set_visible(False)
     
-    handles, labels = ax[0].get_legend_handles_labels()
+    handles, labels = ax[2].get_legend_handles_labels()
     ncol = 1
     bbox_to_anchor = (1.2, 0.5)
     fig.legend(
@@ -195,17 +196,6 @@ def interference_bias_rmse_rr_vs_enum(
                                     (res_df["design"] == "CR")) & 
                                     (res_df["tau_size"] == tau_size) &
                                     (res_df["mirror_type"] == mirror_type)].copy()
-        res_subdf["var"] = res_subdf["rmse"] - res_subdf["bias"]**2
-
-        iter_group_cols = res_subdf.columns[~res_subdf.columns.str.contains("^.*?bias.*?$|^.*?rmse.*?$|^rr.*?$|^var|^design$")].tolist()
-        res_subdf.sort_values(by=iter_group_cols, inplace=True)
-        res_subdf["perc_CR_var"] = (
-            res_subdf.groupby(iter_group_cols, dropna=False)
-            .apply(lambda x: perc_of_benchmark("CR", x, "var"))
-            .reset_index(drop=True)
-            .sort_index()
-            .values
-        )
 
         n_accepts = np.sort(res_df["n_accept"].unique())
         fig, axs = plt.subplots(3, len(n_accepts), figsize=(6*len(n_accepts), 18), sharey='row', sharex='all')